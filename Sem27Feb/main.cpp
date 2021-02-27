1238eac7891fd479d23d3099d7595f7bbc23cae1
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-27 17:20:07 +0300

    Initial Commit

---
+++ Sem27Feb/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Sem27Feb
+//
+//  Created by Alyaev Roman on 27.02.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
